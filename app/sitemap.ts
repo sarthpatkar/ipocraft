0aa7b301271704f931ca90d105fd4b6b8541e064
Sarth Patkar <sarthpatkar70@gmail.com>
2026-03-02 10:11:11 +0530

    advanced sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,31 @@
 import { MetadataRoute } from 'next'
+import { ipos } from '@/data/ipos'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://ipocraft.com',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://ipocraft.com/gmp',
-      lastModified: new Date(),
-    },
+
+  const baseUrl = 'https://ipocraft.com'
+
+  const staticPages = [
+    '',
+    '/gmp',
+    '/ipo',
+    '/ipo-calendar',
+    '/brokers',
+    '/about',
+    '/contact',
+    '/privacy',
+    '/terms',
   ]
+
+  const staticUrls = staticPages.map((path) => ({
+    url: `${baseUrl}${path}`,
+    lastModified: new Date(),
+  }))
+
+  const ipoUrls = ipos.map((ipo) => ({
+    url: `${baseUrl}/ipo/${ipo.slug}`,
+    lastModified: new Date(),
+  }))
+
+  return [...staticUrls, ...ipoUrls]
 }
