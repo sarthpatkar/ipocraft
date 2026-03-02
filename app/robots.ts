bff7b1537208b50c51a79a4416d2b521339869a9
Sarth Patkar <sarthpatkar70@gmail.com>
2026-03-02 10:24:02 +0530

    improve robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/api', '/auth'],
+      },
+    ],
     sitemap: 'https://ipocraft.com/sitemap.xml',
   }
 }
